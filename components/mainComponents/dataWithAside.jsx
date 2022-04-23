d4e315b8929cf8d0c05f06f58403391dbec70dd5
Aditya Kumar <adityakr80510@gmail.com>
2022-04-23 21:00:11 +0530

    BUGS

--- components/mainComponents/dataWithAside.jsx
+++ components/mainComponents/dataWithAside.jsx
@@ -10,7 +10,7 @@ const DataWithAside = (props) => {
           {props.data
             ? dataarr.map((el) => {
                 return (
-                  <div className="group cursor-pointer w-max">
+                  <div key={el.title} className="group cursor-pointer w-max">
                     <h3>{el.link}</h3>
                     <h3 className="text-blue-800 font-medium text-xl group-hover:underline">
                       {el.title}
@@ -22,7 +22,7 @@ const DataWithAside = (props) => {
             : props.code
             ? dataarr.map((el) => {
                 return (
-                  <>
+                  <div key={el.title}>
                     <div
                       className="group cursor-pointer w-max"
                       onClick={() => {
@@ -44,10 +44,10 @@ const DataWithAside = (props) => {
                           version="1.1"
                           width="16"
                           data-view-component="true"
-                          class="octicon octicon-repo mr-1 color-fg-muted"
+                          className="octicon octicon-repo mr-1 color-fg-muted"
                         >
                           <path
-                            fill-rule="evenodd"
+                            fillRule="evenodd"
                             d="M2 2.5A2.5 2.5 0 014.5 0h8.75a.75.75 0 01.75.75v12.5a.75.75 0 01-.75.75h-2.5a.75.75 0 110-1.5h1.75v-2h-8a1 1 0 00-.714 1.7.75.75 0 01-1.072 1.05A2.495 2.495 0 012 11.5v-9zm10.5-1V9h-8c-.356 0-.694.074-1 .208V2.5a1 1 0 011-1h8zM5 12.25v3.25a.25.25 0 00.4.2l1.45-1.087a.25.25 0 01.3 0L8.6 15.7a.25.25 0 00.4-.2v-3.25a.25.25 0 00-.25-.25h-3.5a.25.25 0 00-.25.25z"
                           ></path>
                         </svg>
@@ -61,13 +61,13 @@ const DataWithAside = (props) => {
                       <h3>{el.lang}</h3>
                     </div>
                     <hr className="w-full" />
-                  </>
+                  </div>
                 );
               })
             : props.peoples
             ? dataarr.map((el) => {
                 return (
-                  <>
+                  <div key={el.name}>
                     <div className="group cursor-pointer w-max">
                       <div>
                         <div className="flex">
@@ -88,7 +88,7 @@ const DataWithAside = (props) => {
                       </div>
                     </div>
                     <hr className="w-full" />
-                  </>
+                  </div>
                 );
               })
             : ""}
